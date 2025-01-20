15566e9e63871c52af4465a07a00ab1797a431f1
j-dupon <stij95@zedat.fu-berlin.de>
2025-01-20 15:29:56 +0100

    Improve LQI imports

--- src/BLUME/sync_blume.py
+++ src/BLUME/sync_blume.py
@@ -35,19 +35,25 @@ def import_Location(station):
 		Things = []
 		)
 
-def import_ObservedProperty(data): 
-	res = requests.get(f"https://luftdaten.berlin.de/api/cores/{data['core']}")
-	core = res.json() if res.ok else {"name": data["core"], "description": f'{data["component"]}-{data["component"]}'}
+def import_ObservedProperty(data, details): 
+	if data["core"] in details:
+		symbol = details[data["core"]]["unit"]
+		description = details[data["core"]]["description"]
+		definition = f"https://luftdaten.berlin.de/pollution/{data['core']}"
+	else:
+		symbol = ""
+		description = "Luftqualitätsindex"
+		definition = f"https://luftdaten.berlin.de/lqi/info"
 
 	return sensorThings_entities.ObservedProperty({ 
 		"name": data["core"],
-		"description": core["name"],
+		"description": description[:255-3] + '...' if len(description) > 255 else description,
 		"properties": {
-			"symbol": "",
+			"symbol": symbol,
 			"component": data["component"],
     	"period": data["period"],
 		},
-		"definition": core["description"],
+		"definition": definition,
 	})
 
 def import_Sensor():
@@ -58,7 +64,7 @@ def import_Sensor():
 	  "metadata": "https://www.berlin.de/sen/uvk/umwelt/luft/luftqualitaet/berliner-luft/messnetz/"
 	})
 
-def import_Datastream(observed_property, thing, sensor):
+def import_Datastream(observed_property, sensor, thing):
 	return sensorThings_entities.Datastream(
 		observed_property, 
 		sensor, 
@@ -67,36 +73,35 @@ def import_Datastream(observed_property, thing, sensor):
 
 def sync(things, sensor):
 	LOGGER.log.info(f"\n\n########## Start BLUME synchronization ##########\n")
+	update_count = 0
 
 	stations = requests.get(f"https://luftdaten.berlin.de/api/stations")
 	res = requests.get(f"https://luftdaten.berlin.de/api/lqis/data")
 	lqi_stations = [lqi_station["station"] for lqi_station in res.json()]
 	lqi_data = [lqi_station["data"] for lqi_station in res.json()]
-	
-	update_count = 0
+	details = {}
 
 	for station in stations.json():
 		LOGGER.log.info(f"### Start synchronization for >>{'active' if station['active'] else 'inactive'}<< station({station['name']}) ###")
 
-		# Import BLUME instance and location if not exists
-		if station["code"] not in things:
-			things[station["code"]] = import_Thing(station)
-			location = import_Location(station)
-			location.link_to_things([{"@iot.id": things[station["code"]].iot_id()}])
-			location.update_self()
-
-		thing = things[station["code"]]
-			
-		# Import observation
 		if station["active"]:
 			update_count += 1
+
+			# Import BLUME thing and location if not exists
+			if station["code"] not in things:
+				things[station["code"]] = import_Thing(station)
+				location = import_Location(station)
+				location.link_to_things([{"@iot.id": things[station["code"]].iot_id()}])
+				location.update_self()
+			
 			res = requests.get(f"https://luftdaten.berlin.de/api/stations/{station['code']}/data")
 			station_data = res.json()
+			thing = things[station["code"]]
 
 			# Merge lqi data
 			if station["code"] in lqi_stations:
 				for data in lqi_data[lqi_stations.index(station["code"])]:
-					data["core"] = "lqi_" + data["component"]
+					data["core"] = "lqi" if data["component"] == "lqi" else f"lqi({data['component']})"
 					station_data.append(data)
 
 			datastreams = [datastream["unitOfMeasurement"]["name"] for datastream in thing.datastreams()]
@@ -110,8 +115,11 @@ def sync(things, sensor):
 
 				# Make sure that the datastream and observed property exists
 				if data["core"] not in datastreams:
-					observed_property = import_ObservedProperty(data)
-					datastream = import_Datastream(observed_property, thing, sensor)
+					if details ==  {}:
+						res = requests.get(f"https://luftdaten.berlin.de/api/components")
+						[details.update({component["core"]: component}) for component in res.json()]
+					observed_property = import_ObservedProperty(data, details)
+					datastream = import_Datastream(observed_property, sensor, thing)
 					datastream_iot_id = datastream.iot_id()
 				else:
 					datastream_iot_id = [datastream["@iot.id"] for datastream in thing.datastreams() if datastream["unitOfMeasurement"]["name"] == data["core"]][0]
