f6bd0e4d6db5f7128ec25907a1fc51c83298e118
j-dupon <stij95@zedat.fu-berlin.de>
2025-01-27 17:47:12 +0100

    Vereinfachung der Telraam sync, archivierung von things/locations, fallback api key

--- src/Telraam/TelraamAPI.py
+++ src/Telraam/TelraamAPI.py
@@ -6,9 +6,15 @@ LOGGER = logger.log
 
 class TelraamAPI:
 
-	def __init__(self, api_key_header, base_url):
-		self.api_key_header = api_key_header
+	def __init__(self, api_key, api_key_fallback, base_url):
+		self.api_key = api_key
+		self.api_key_fallback = api_key_fallback
+		self.api_key_header = {
+			"X-Api-Key": self.api_key,
+			"Content-Type": "application/json"
+			}
 		self.base_url = base_url
+		self.request_counter = 0
 
 	def get_traffic_snapshot(self, settings):
 		url = f"{self.base_url}/reports/traffic_snapshot"
@@ -42,23 +48,39 @@ class TelraamAPI:
 	def get_segment_by_id(self, segment_id):
 		url = f"{self.base_url}/segments/id/{segment_id}"
 		return self.telraam_get(url)
+	
+	def swap_api_key(self):
+		self.request_counter = 0
+		LOGGER.err.warning(f"swapped the X-Api-Key after {self.request_counter} requests")
+		if self.api_key_header["X-Api-Key"] == self.api_key:
+			self.api_key_header["X-Api-Key"] = self.api_key_fallback
+		else:
+			self.api_key_header["X-Api-Key"] = self.api_key
+
 
 	def telraam_get(self, url):
 		try:
 			res = requests.get(url, headers = self.api_key_header)
 			if res.status_code > 200:
+				self.swap_api_key(self)
 				return {"ok": 0, "error_message": res.json()}
+			self.request_counter += 1
 			return {"ok": 1, "result": res}
 		except RuntimeError as err:
+			self.swap_api_key(self)
 			LOGGER.err.error(f"ERROR -> telraam_get: {err}")
 			return {"ok": 0, "error_message": err}
 
 	def telraam_post(self, url, body):
+		self.request_counter += 1
 		try:
 			res = requests.post(url, data = json.dumps(body), headers = self.api_key_header)
 			if res.status_code > 200:
+				self.swap_api_key(self)
 				return {"ok": 0, "error_message": res.json()}
+			self.request_counter += 1
 			return {"ok": 1, "result": res}
 		except RuntimeError as err:
+			self.swap_api_key(self)
 			LOGGER.err.error(f"ERROR -> telraam_post: {err}")
 			return {"ok": 0, "error_message": err}
